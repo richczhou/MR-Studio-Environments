138f2bb823626ac140dec66ba55b51bb3457fa18
Richard Zhou <richczhou@gmail.com>
2020-10-18 18:50:45 -0400

    Added gaze functionality

---
+++ Assets/Scripts/GazeInteraction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GazeInteraction : MonoBehaviour
+{
+    List<InfoBehavior> infos = new List<InfoBehavior>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        infos = FindObjectsOfType<InfoBehavior>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit))
+        {
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("hasInfo"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+            }
+        }
+        else
+        {
+            CloseAllInfos();
+        }
+    }
+
+    public void OpenInfo(InfoBehavior desiredInfo)
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            if(info == desiredInfo) info.OpenInfo();
+            else info.CloseInfo();
+        }
+    }
+
+    public void CloseAllInfos()
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            info.CloseInfo();
+        }
+    }
+}
---
+++ Temp/il2cppOutput/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,115 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void ARTapToPlaceObject::Start()
+extern void ARTapToPlaceObject_Start_mCEA5A9ADC5ACD785F329254FD679C5513136EDD4 (void);
+// 0x00000002 System.Void ARTapToPlaceObject::Update()
+extern void ARTapToPlaceObject_Update_mCEA015466DB37FE1DC8947F8B16E5773004E3F7C (void);
+// 0x00000003 System.Void ARTapToPlaceObject::PlaceObject()
+extern void ARTapToPlaceObject_PlaceObject_m72557EDFCEA7F7FDBE5E40E5ADE990B91FCB2EA7 (void);
+// 0x00000004 System.Void ARTapToPlaceObject::UpdatePlacementIndicator()
+extern void ARTapToPlaceObject_UpdatePlacementIndicator_m03B360FB4F2108EE4568BEDD76425EEB98FC2B9C (void);
+// 0x00000005 System.Void ARTapToPlaceObject::UpdatePlacementPose()
+extern void ARTapToPlaceObject_UpdatePlacementPose_m97C991933A4B270119305A0974E6D69D45D6640D (void);
+// 0x00000006 System.Void ARTapToPlaceObject::.ctor()
+extern void ARTapToPlaceObject__ctor_mF5B2CD917913D8081CB42BA882632868D038267B (void);
+// 0x00000007 System.Void FaceCamera::Start()
+extern void FaceCamera_Start_mDB7CD5467FA3C6A0466CD9AEC8954A45928D19F5 (void);
+// 0x00000008 System.Void FaceCamera::Update()
+extern void FaceCamera_Update_mEE71D4EA0FF72FDFB94526D743349EBFEF318022 (void);
+// 0x00000009 System.Void FaceCamera::.ctor()
+extern void FaceCamera__ctor_m8A7DBB82F377E94201A154D42DDDA6FAA50145A8 (void);
+// 0x0000000A System.Void GazeInteraction::Start()
+extern void GazeInteraction_Start_mD4AB0481E644D04937E91638B8F461B79DDDB5A4 (void);
+// 0x0000000B System.Void GazeInteraction::Update()
+extern void GazeInteraction_Update_mCDF9FE6EE4A47DBCBB43D6BD991B6096C7EBFC1F (void);
+// 0x0000000C System.Void GazeInteraction::OpenInfo(InfoBehavior)
+extern void GazeInteraction_OpenInfo_m994B1DB97E8AA1082FC61AABD2B86C816943CF58 (void);
+// 0x0000000D System.Void GazeInteraction::CloseAllInfos()
+extern void GazeInteraction_CloseAllInfos_m9B0732BA25121E91906B2349A5B8DC281CB4905F (void);
+// 0x0000000E System.Void GazeInteraction::.ctor()
+extern void GazeInteraction__ctor_m10530F9E0D57F6492BC94857C75116E37B2FFD30 (void);
+// 0x0000000F System.Void InfoBehavior::Update()
+extern void InfoBehavior_Update_m25879CA886485D086070DA4E93CA1DE8C5D8FE1A (void);
+// 0x00000010 System.Void InfoBehavior::OpenInfo()
+extern void InfoBehavior_OpenInfo_mC31D314AD78C6484FFF93D9F56FEB5F6EE92E99D (void);
+// 0x00000011 System.Void InfoBehavior::CloseInfo()
+extern void InfoBehavior_CloseInfo_m1DF9521B5F0EF99198C7C93003E2FB0D2C76D96E (void);
+// 0x00000012 System.Void InfoBehavior::.ctor()
+extern void InfoBehavior__ctor_mF50434E6291E6FDD161DC514205D259EF293C96D (void);
+static Il2CppMethodPointer s_methodPointers[18] = 
+{
+	ARTapToPlaceObject_Start_mCEA5A9ADC5ACD785F329254FD679C5513136EDD4,
+	ARTapToPlaceObject_Update_mCEA015466DB37FE1DC8947F8B16E5773004E3F7C,
+	ARTapToPlaceObject_PlaceObject_m72557EDFCEA7F7FDBE5E40E5ADE990B91FCB2EA7,
+	ARTapToPlaceObject_UpdatePlacementIndicator_m03B360FB4F2108EE4568BEDD76425EEB98FC2B9C,
+	ARTapToPlaceObject_UpdatePlacementPose_m97C991933A4B270119305A0974E6D69D45D6640D,
+	ARTapToPlaceObject__ctor_mF5B2CD917913D8081CB42BA882632868D038267B,
+	FaceCamera_Start_mDB7CD5467FA3C6A0466CD9AEC8954A45928D19F5,
+	FaceCamera_Update_mEE71D4EA0FF72FDFB94526D743349EBFEF318022,
+	FaceCamera__ctor_m8A7DBB82F377E94201A154D42DDDA6FAA50145A8,
+	GazeInteraction_Start_mD4AB0481E644D04937E91638B8F461B79DDDB5A4,
+	GazeInteraction_Update_mCDF9FE6EE4A47DBCBB43D6BD991B6096C7EBFC1F,
+	GazeInteraction_OpenInfo_m994B1DB97E8AA1082FC61AABD2B86C816943CF58,
+	GazeInteraction_CloseAllInfos_m9B0732BA25121E91906B2349A5B8DC281CB4905F,
+	GazeInteraction__ctor_m10530F9E0D57F6492BC94857C75116E37B2FFD30,
+	InfoBehavior_Update_m25879CA886485D086070DA4E93CA1DE8C5D8FE1A,
+	InfoBehavior_OpenInfo_mC31D314AD78C6484FFF93D9F56FEB5F6EE92E99D,
+	InfoBehavior_CloseInfo_m1DF9521B5F0EF99198C7C93003E2FB0D2C76D96E,
+	InfoBehavior__ctor_mF50434E6291E6FDD161DC514205D259EF293C96D,
+};
+static const int32_t s_InvokerIndices[18] = 
+{
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	26,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	18,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
